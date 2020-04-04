91437c9fc7258145ac5fabd80bbd08136afb3c8d
halimarefat@gmail.com <halimarefat@gmail.com>
2020-04-04 15:56:32 +0430

    adding python file of simple linear regression example using FuelConsumption.csv dataset.

---
+++ simpleLinReg.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import r2_score
+
+df = pd.read_csv("FuelConsumption.csv")
+print(df.head())
+print(df.describe())
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+print(cdf.head(9))
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
+
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS, color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color='blue')
+plt.xlabel("Cylinders")
+plt.ylabel("Emission")
+plt.show()
+
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='red')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+
+print('Coefficients: ', regr.coef_)
+print('Intercept: ', regr.intercept_)
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='red')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-k')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+pred_y = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(pred_y - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean(np.absolute(pred_y - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(pred_y, test_y))
